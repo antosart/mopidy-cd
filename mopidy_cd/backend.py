f7310f092f4cd23a32bb7e1996ef09402c1ed8da
Antonio <anton.sartori@gmail.com>
2017-05-25 15:15:17 +0200

    Fix bug causing exception when no disc is inserted
    This commit fixes #8

--- mopidy_cd/backend.py
+++ mopidy_cd/backend.py
@@ -15,23 +15,24 @@ class CdBackend(pykka.ThreadingActor, backend.Backend):
         super(CdBackend, self).__init__()
         self.audio = audio
         self.cdrom = cdrom.Cdrom()
-        
+
         self.library = CdLibrary(backend=self)
         self.playback = CdPlaybackProvider(audio=audio, backend=self)
-        
+
+
 class CdLibrary(backend.LibraryProvider):
     root_directory = Ref.directory(uri='cd:root', name='Cd')
 
     def browse(self, uri):
         self.refresh()
         results = []
-        if not uri=='cd:root':
+        if not uri == 'cd:root':
             return results
-        logger.debug('Cdrom backend %s',self.backend)
+        logger.debug('Cdrom backend %s', self.backend)
         tracks = self.backend.cdrom.tracks
-        logger.debug('Cdrom: in browse found %d tracks',len(tracks))
-        for (seq,(number,name,duration,albumtitle,genre,year)) in enumerate(tracks,1):
-            results.append(Ref.track(uri='cd:/'+str(seq), name=name))
+        logger.debug('Cdrom: in browse found %d tracks', len(tracks))
+        for (seq, (number, name, duration, albumtitle, genre, year)) in enumerate(tracks, 1):
+            results.append(Ref.track(uri='cd:/' + str(seq), name=name))
         return results
 
     def refresh(self, uri=None):
@@ -39,22 +40,24 @@ class CdLibrary(backend.LibraryProvider):
 
     def lookup(self, uri):
         logger.debug('Cdrom: track selected')
-        i=int(uri.lstrip("cd:/"))-1
-        logger.debug('Cdrom: track %s selected',i)
-        (number,name,duration,albumtitle,genre,year) = self.backend.cdrom.tracks[i]
-        album = Album(name = albumtitle)
+        i = int(uri.lstrip("cd:/")) - 1
+        logger.debug('Cdrom: track %s selected', i)
+        (number, name, duration, albumtitle,
+         genre, year) = self.backend.cdrom.tracks[i]
+        album = Album(name=albumtitle)
         return [Track(uri=uri,
                       name=name,
-                      length=int(duration)*1000,
+                      length=int(duration) * 1000,
                       genre=genre,
                       date=year,
                       album=album)]
-        
+
+
 class CdPlaybackProvider(backend.PlaybackProvider):
-    
+
     def change_track(self, track):
         logger.debug('Cdrom: playing track %s', track)
         return super(CdPlaybackProvider, self).change_track(track)
 
     def translate_uri(self, uri):
-        return uri.replace('cd:/','cdda://')
+        return uri.replace('cd:/', 'cdda://')
--- mopidy_cd/cdrom.py
+++ mopidy_cd/cdrom.py
@@ -3,29 +3,44 @@ from __future__ import unicode_literals
 import logging
 import time
 import discid
-import CDDB, DiscID
+try:
+    import CDDB
+except ImportError:
+    logger.info('CDDB not found. Tracks info will not be loaded from cddb')
+try:
+    import DiscID
+except ImportError:
+    logger.info('DiscID not found. Tracks info will not be loaded from cddb')
 import unicodedata
 
 logger = logging.getLogger(__name__)
+
 
 class Cdrom(object):
 
     def __init__(self):
         self.refresh()
-    
-    def sanitizeString(self, string):
+
+    @staticmethod
+    def sanitizeString(string):
         return unicode(string.decode('iso-8859-1').encode('utf-8'), 'utf-8')
 
     def refresh(self):
-        self.disc = discid.read()
+        self.tracks = []
+        try:
+            self.disc = discid.read()
+        except:
+            logger.debug("Cdrom: Unable to read cd")
+            return
         logger.debug("Cdrom: reading cd")
         self.n = len(self.disc.tracks)
-        logger.debug('Cdrom: %d tracks found',self.n)
+        logger.debug('Cdrom: %d tracks found', self.n)
         read_info = {}
         try:
             self.disc_id = DiscID.disc_id(DiscID.open())
             (query_status, query_info) = CDDB.query(self.disc_id)
-            (read_status, read_info) = CDDB.read(query_info['category'], query_info['disc_id'])
+            (read_status, read_info) = CDDB.read(
+                query_info['category'], query_info['disc_id'])
         except:
             pass
         if 'DYEAR' in read_info:
@@ -40,13 +55,21 @@ class Cdrom(object):
             self.albumtitle = self.sanitizeString(read_info['DTITLE'])
         else:
             self.albumtitle = 'CD'
-        self.tracks=[]
         for track in self.disc.tracks:
             number = track.number
             duration = track.seconds
-            key = 'TTITLE' + `(track.number-1)`
+            key = 'TTITLE' + repr((track.number - 1))
             if key in read_info:
                 name = self.sanitizeString(read_info[key])
             else:
-                name = 'Cdrom Track %s (%s)' % (number, time.strftime('%H:%M:%S', time.gmtime (duration)))
-            self.tracks.append((number,name,duration,self.albumtitle,self.genre,self.year))
+                name = 'Cdrom Track %s (%s)' % (
+                    number,
+                    time.strftime('%H:%M:%S',
+                                  time.gmtime(duration)))
+            self.tracks.append(
+                (number,
+                 name,
+                 duration,
+                 self.albumtitle,
+                 self.genre,
+                 self.year))
